f7a6ff051a2370c2cec2fa774e69fbea61bc586d
Joe D'Amore <joe@joedamore.me>
2020-05-25 18:24:53 -0400

    Export mediaArtwork model with other models

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,13 @@
 const collection = require('./collection.js');
 const media = require('./media.js');
+const mediaArtwork = require('./mediaArtwork.js');
 const mediaMetadata = require('./mediaMetadata.js');
 const user = require('./user.js');
 
 module.exports = {
   collection,
   media,
+  mediaArtwork,
   mediaMetadata,
   user,
 };
