51a79fde7d6af9062515344488f0aa5fa6a017ae
Joe D'Amore <joe@joedamore.me>
2020-05-28 10:18:44 -0400

    Add association between "Collection" model and "CollectionCollection" and "CollectionMedia" models

--- src/models/collection.js
+++ src/models/collection.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 const mediaDb = require('../db/mediaDb.js');
 
+const CollectionCollections = mediaDb.models.CollectionCollections;
+
 class Collection extends Sequelize.Model {};
 
 Collection.init({
@@ -8,17 +10,20 @@ Collection.init({
     type: Sequelize.STRING,
     allowNull: false,
   },
-  parentCollection: {
-    type: Sequelize.INTEGER,
-    allowNull: true,
-    references: {
-      model: 'collections',
-      key: 'id',
-    }
-  },
+  //parentCollection: {
+  //  type: Sequelize.INTEGER,
+  //  allowNull: true,
+  //  references: {
+  //    model: 'collections',
+  //    key: 'id',
+  //  }
+  //},
 }, {
   sequelize: mediaDb,
   modelName: 'collection',
 });
+
+Collection.hasMany('collectionCollections', {foreignKey: 'childCollectionId', as: 'collections' });
+Collection.hasMany('collectionMedia', {foreignKey: 'childMediaId', as: 'media'});
 
 module.exports = Collection;
