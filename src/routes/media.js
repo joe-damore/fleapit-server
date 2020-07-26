3a6a9f0a194ee96e2a6f23483631dd193ae59553
Joe D'Amore <joe@joedamore.me>
2020-07-25 22:39:04 -0400

    Improve media routes

--- src/routes/media.js
+++ src/routes/media.js
@@ -8,16 +8,13 @@ router.post('/', mediaController.createMedia);
 
 router.get('/:id', mediaController.findMedia);
 router.delete('/:id', mediaController.deleteMedia);
-// TODO Delete "ext" route
-// router.get('/:id/ext', mediaController.findMediaExtended);
+
 router.get('/:id/view', mediaController.viewMedia);
-// router.get('/:id/view', mediaController.viewMedia);
-// router.get('/:id/download', mediaController.downloadMedia);
+router.get('/:id/download', mediaController.downloadMedia);
 
 router.get('/:id/metadata', mediaController.findMediaMetadata);
-router.post('/:id/metadata', mediaController.updateMediaMetadata);
-// router.delete('/:id/metadata', mediaController.deleteMediaMetadata);
-// TO DELETE router.post('/:id/metadata', mediaController.replaceMediaMetadata);
+router.post('/:id/metadata', mediaController.upsertMediaMetadata);
+router.patch('/:id/metadata', mediaController.patchMediaMetadata);
 
 router.get('/:id/artwork', mediaController.findMediaArtwork);
 router.post('/:id/artwork', mediaController.createMediaArtwork);
