aa36d677cd349c1be73377259a2ca2cc4f4a6871
Joe D'Amore <joe@joedamore.me>
2020-07-25 22:38:13 -0400

    Document test

--- test/util/scrub.spec.js
+++ test/util/scrub.spec.js
@@ -2,9 +2,23 @@ const { expect } = require('chai');
 const scrub = require('../../src/util/scrub.js');
 
 describe('./src/util/scrub.js', function() {
+
+  /*
+   * The scrub function's purpose is to remove unnecessary or sensitive fields
+   * from an object, typically so that it can then safely be served to the
+   * client.
+   *
+   * These tests ensure that this function behaves as expected.
+   */
   describe('scrub()', function() {
 
+    /*
+     * Ensures that all of the specified properties are scrubbed from the
+     * input object.
+     */
     it('should remove specified properties from given object', function() {
+
+      // These properties should be scrubbed from the input object.
       const scrubbedProperties = [
         'propertyA',
         'propertyB',
@@ -24,7 +38,14 @@ describe('./src/util/scrub.js', function() {
       expect(result).to.have.property('propertyC').that.eql(undefined);
     });
 
+
+    /*
+     * Ensures that unspecified properties do not get scrubbed from the input
+     * object.
+     */
     it('should not remove unspecified properties from given object', function() {
+
+      // These properties should be scrubbed from the input object.
       const scrubbedProperties = [
         'propertyA',
         'propertyB',
@@ -43,6 +64,10 @@ describe('./src/util/scrub.js', function() {
       expect(result).to.have.property('propertyE').that.eql(100);
     });
 
+    /*
+     * Ensures that the input object is unmodified when no properties are
+     * specified to scrub.
+     */
     it('should not modify given object when no properties are specified', function() {
       const scrubbedProperties = [];
 
